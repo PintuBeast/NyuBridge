08bc831ac5864370650dd4aa0e61ace9376eb69d
pintubeast <abhishek.jk1992@gmail.com>
2023-07-18 08:14:24 +0530

    first code done

--- aj4119_hw2_q1.cpp
+++ aj4119_hw2_q1.cpp
@@ -1,6 +1,23 @@
+// Author: Abhishek Jayakumar Kurup
+// Last updated: 2023-07-17
 #include<iostream>
 using namespace std;
+int numPennies,numQuarters,numDimes, numNickels;
+int sumInPennies,numDollars,numCents;
 int main()
 {
-    int 
+    cout<<"Please enter the number of coins\n";
+    cout<<"# of quarters: ";
+    cin>>numQuarters;
+    cout<<"# of dimes: ";
+    cin>>numDimes;
+    cout<<"# of nickels: ";
+    cin>>numNickels;
+    cout<<"# of pennies: ";
+    cin>>numPennies;  
+    sumInPennies=numQuarters*25+numDimes*10+numNickels*5+numPennies;
+    numDollars=sumInPennies/100;
+    numCents=sumInPennies%100;   
+    cout<<"The total is "<<numDollars<<" dollars and "<<numCents<<" cents\n"; 
+    return 0;  
 }
---
+++ aj4119_hw2_q2.cpp
@@ -0,0 +1,12 @@
+// Author: Abhishek Jayakumar Kurup
+// Last updated: 2023-07-17
+#include<iostream>
+using namespace std;
+int x,y;
+int main()
+{
+    cout<<"Enter two numbers";
+    cin>>x>>y;
+    cout<<"X is: "<<x<<"Y is:"<<y<<endl;
+    return 0;
+}
