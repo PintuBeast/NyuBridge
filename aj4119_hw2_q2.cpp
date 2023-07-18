2f9a57d0c55b3e2fd38de1b531a3b6c65ff501e4
pintuBeast <abhishek.jk1992@gmail.com>
2023-07-18 11:45:13 +0530

    Second Homework completed

--- aj4119_hw2_q2.cpp
+++ aj4119_hw2_q2.cpp
@@ -2,11 +2,19 @@
 // Last updated: 2023-07-17
 #include<iostream>
 using namespace std;
-int x,y;
+int numDollars,numCents,sumInPennies;
+int numPennies,numQuarters,numDimes, numNickels;
 int main()
 {
-    cout<<"Enter two numbers";
-    cin>>x>>y;
-    cout<<"X is: "<<x<<"Y is:"<<y<<endl;
+    cout<<"Please enter your amount in the format of dollars and cents seperated by a space:\n";
+    cin>>numDollars>>numCents;
+    sumInPennies=numDollars*100+numCents;
+    numQuarters=sumInPennies/25;
+    numDimes=(sumInPennies%25)/10;
+    numNickels=((sumInPennies%25)%10)/5;
+    numPennies=((sumInPennies%25)%10)%5;
+    cout<<numDollars<<" dollars and "<<numCents<<" cents are:\n"
+        <<numQuarters<<" quarters, "<<numDimes<<" dimes, "
+        <<numNickels<<" nickels and "<<numPennies<<" pennies";
     return 0;
 }
